eee697295d261170872bf5a2dc0722908746e027
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-09-23 16:40:56 -0700

    added a tool for converting backup ephem files from esrange to something useable

---
+++ utils/ESRANGE_GPS_convert.js
@@ -0,0 +1,70 @@
+"use strict";
+var J2000 = +(new Date("2000-01-01T11:58:55.816Z"));
+var fs = require("fs");
+var inFile = process.argv[2];
+var outFile = process.argv[3] || inFile + ".out";
+
+fs.readFile(inFile, "utf8", function(err, data) {
+  let coords = [];
+  if (err) {
+    console.log(err);
+    return;
+  }
+  
+  data.split("\n").forEach((line) => {
+    let values, date, alt, lat, lon;
+    
+    //skip blank lines
+    line = line.trim();
+    if (!line.length) {return;}
+
+    //date is separated from the rest of the csv line with a ;
+    values = line.split(";");
+    date = values[0];
+    //reformat the date as ns since J2000
+    date = dateToJ2000(date);
+
+    //break down the rest of the csv values
+    values = values[1].split(",");
+    
+    //convert lat from DDmm.mm to DD.DD
+    lat = +values[6].substr(0, 2);
+    lat += (+values[6].substr(2)/60);
+    
+    //lat direction must be N
+    if (values[7] !== "N") {
+      lat *= -1;
+    }
+
+    //convert lon from DDmm.mm to DD.DD
+    lon = +values[8].substr(0, 2);
+    lon += (+values[8].substr(2)) / 60;
+    
+    //lon direction must be E 
+    if (values[9] !== "E") {
+      lon *= -1;
+    }
+
+    //convert alt from m to km
+    alt = (+values[10]) / 1000;
+
+    coords.push(date + "," + lat + "," + lon + "," + alt);
+  }, this);
+
+  fs.writeFile(outFile, coords.join("\n"));
+});
+
+function dateToJ2000(date) {
+  if (typeof date != "string") {
+    return 0;
+  }
+
+  //format the date string into a UTC time
+  date = date.replace(/ /, "T");
+  date = date + "Z";
+
+  //convert to a js object
+  date = new Date(date);
+  
+  return ((+date) - J2000); 
+}
